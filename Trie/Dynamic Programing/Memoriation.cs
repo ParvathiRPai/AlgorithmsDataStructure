74a55ba23be4de035617f0476eb28794a678e84f
parvathi pai <parvathirpai@outlook.com>
2018-11-14 14:18:00 -0800

    Dynamic Programing

---
+++ Trie/Dynamic Programing/Memoriation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trie
+{
+    class Something
+    {
+        //used to store distinct requests - those with a currentTotal, and numDecisions, because the other 2 are not function specific - choicesPerDecision does not change ever, and allTotals is just used for record-keeping, it has no purpose in the recursion
+        //so the cardinality (things that change for this function has 2 params) and each is finite.
+        //If things are finite, you can use 1) memoization 2) DP
+        private HashSet<Tuple<int, int>> Memo = new HashSet<Tuple<int, int>>();
+
+        //Enumerate also means Find All
+        //FindDeductive... or FindSubtractive... means currentTotal starts at a large number instead of 0, the number of decisions and choices per decisions does not change and its values depends on problem
+        //all totals is a record-keeper - keeps track of all totals (additive or subtractive) that have been found so far, always starts empty
+        //numDecisions is the number of decisions you will still have to do in the future, this will always be large in the beginning of the problem
+        public void FindAdditiveResultsWithDecisionsAndChoices(int numDecisions, int[] choicesPerDecision, int currentTotal, HashSet<int> allTotals)
+        {
+            if(numDecisions == 0)
+            {
+                //check if value exists in allTotals
+                if(!allTotals.Contains(currentTotal))
+                {
+                    allTotals.Add(currentTotal);
+                    return;
+                }
+
+                return;
+            }
+
+            foreach(var choice in choicesPerDecision)
+            {
+                //optional memoization
+                //memoization is basically storing requests (methods in a console app, or even web requests for static services, also known as static caching
+                //1) add a HashSet to store requests in the class
+                //recursively call but with fewer future decisions - allTotals and choicesPerDecision are references that do not change
+                //check before recursion if that has already been called
+                if(Memo.Contains(Tuple.Create(numDecisions - 1, currentTotal + choice)))
+                {
+                    continue;
+                }
+                else
+                {
+                    Memo.Add(Tuple.Create(numDecisions - 1, currentTotal + choice));
+                }
+
+                FindAdditiveResultsWithDecisionsAndChoices(numDecisions - 1, choicesPerDecision, currentTotal + choice, allTotals);
+            }
+        }
+    }
+}
---
+++ Trie/Node.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trie
+{
+    public class Node<TValue>
+    {
+        public const int CharCount = 256;
+        //used for all off-line algorithms - algorithms where the entire input is known from the start (IP addesses etc.)
+        public char TrieData { get; set; }
+        public TValue FinalValue { get; set; }
+        public Node<TValue>[] Children = new Node<TValue>[CharCount];
+        //only use for trie-based prediction algorithms
+        internal int Frequency { get; set; }
+        internal IDictionary<int, int> FrequencySentenceCountMap = new Dictionary<int, int>();
+    }
+
+    public interface ITrie<TValue>
+    {
+        //same as add - but this is not an IList
+        //Updates the trie using the new sentence
+        void Feed(string input, TValue value);
+        //gets all sentences that match the input, or uses prediction, if available 
+        TValue Get(string input);
+        bool UsePrediction { get; }
+    }
+
+    public class Trie<TValue> : ITrie<TValue>
+    {
+        private Node<TValue> _root = null;
+
+        public bool UsePrediction => false;
+             
+        private IList<char> GetCharArr(string input)
+        {
+            var charArr = new List<char>();
+            charArr.AddRange(input);
+            charArr.InsertRange(0, new List<char>
+            {
+                ' ',
+            });
+            return charArr;
+        }
+
+        public void Feed(string input, TValue value)
+        {
+            input = input.Trim();
+            var charArr = GetCharArr(input);
+            if(_root == null)
+            {
+                _root = new Node<TValue>();
+            }
+
+            FeedAt(_root, charArr, value);
+        }
+
+        private static void FeedAt(Node<TValue> location, IList<char> arr, TValue value)
+        {
+            //arr should not be null
+            if(arr.Count == 0)
+            {
+                return;
+            }
+
+            //location should not be null
+            if (location == null)
+            {
+                location = new Node<TValue>
+                {
+                    TrieData = arr[0]
+                };
+            }
+            else
+            {
+                //redundant update because this might have been done earlier by another sentence
+                location.TrieData = arr[0];
+            }
+
+            //remove added element
+            arr.RemoveAt(0);
+            
+            //if no more, set final value
+            if(arr.Count == 0)
+            {
+                location.FinalValue = value;
+                return;
+            }
+
+            //recurse for other chars in input
+            int charIndex = (int)arr[0];
+            if(location.Children[charIndex] == null)
+            {
+                location.Children[charIndex] = new Node<TValue>();
+            }
+
+            FeedAt(location.Children[charIndex], arr, value);
+        }
+
+        public TValue Get(string input)
+        {
+            var charArr = GetCharArr(input);
+            return GetAt(_root, charArr);
+        }
+
+        private static TValue GetAt(Node<TValue> location, IList<char> charArr)
+        {
+            if (location == null)
+            {
+                return default(TValue);
+            }
+
+            if (charArr == null || charArr.Count == 0)
+            {
+                return default(TValue);
+            }
+
+            if (location.TrieData != charArr[0])
+            {
+                return default(TValue);
+            }
+
+            charArr.RemoveAt(0);
+
+            if(charArr.Count == 0)
+            {
+                return location.FinalValue;
+            }
+
+            return GetAt(location.Children[(int)charArr[0]], charArr);
+        }
+    }
+}
